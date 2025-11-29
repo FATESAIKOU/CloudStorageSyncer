a5836b86adf9ea2c695a24f1bddb0f706e10e0df
FATESAIKOU <qzsecftbhhhh@gmail.com>
2025-11-29 23:05:52 +0900

    Fix

--- docker/nginx-entrypoint.sh
+++ docker/nginx-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 使用環境變數替換 nginx 配置中的變數
-envsubst '${BACKEND_HOST} ${BACKEND_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${BACKEND_HOST} ${BACKEND_PORT} ${MAX_UPLOAD_SIZE}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 # 啟動 nginx
 exec nginx -g 'daemon off;'
